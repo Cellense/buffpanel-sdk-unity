8b88deabd28c8c922d62826c73c906be1fe4d519
Osama Hassanein <player1os@gmail.com>
2017-03-17 06:53:58 +0100

    Refactor sender coroutine

--- Assets/Scripts/BuffPanelSDK/BuffPanel.cs
+++ Assets/Scripts/BuffPanelSDK/BuffPanel.cs
@@ -1,7 +1,7 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using System.Text;
+using UnityEngine;
 
 namespace BuffPanel
 {
@@ -17,14 +17,15 @@ namespace BuffPanel
         private static float initialRetryOffset = 0.25f;
 		private static int maxRetryCount = 10;        
 
-        private static Dictionary<string, string> _httpHeaders = new Dictionary<string, string>
-        {
+        private static Dictionary<string, string> httpHeaders = new Dictionary<string, string> {
             { "Content-type", "application/json" }
         };
 
 		public static void Track(string gameToken, string playerToken, Callback callback = null)
 		{
-			Track(gameToken, new Dictionary<string, object> { { "registered", playerToken } }, callback);
+			Track(gameToken, new Dictionary<string, object> {
+				{ "registered", playerToken }
+			}, callback);
 		}
 
 		public static void Track(string gameToken, Dictionary<string, object> playerTokens, Callback callback = null)
@@ -32,64 +33,59 @@ namespace BuffPanel
             string url = "http://" + serviceHostname + servicePath;
 
 			Dictionary<string, object> playerTokensDict = new Dictionary<string, object> ();
-			if (playerTokens.ContainsKey("registered"))
-            {
+			if (playerTokens.ContainsKey("registered")) {
 				playerTokensDict.Add("registered", playerTokens["registered"]);
 			}
-			if (playerTokens.ContainsKey("user_id"))
-            {
+			if (playerTokens.ContainsKey("user_id")) {
 				playerTokensDict.Add("user_id", playerTokens["user_id"]);
 			}
-			if (playerTokensDict.Count == 0)
-            {
+			if (playerTokensDict.Count == 0) {
 				callback.error(null);
 				return;
 			}
 
-            string httpBody = Json.Serialize(new Dictionary<string, object>
-            {
+            string httpBody = Json.Serialize(new Dictionary<string, object> {
                 { "game_token", gameToken },
 				{ "player_tokens", playerTokensDict }
-                // { "browser_cookies", CookieExtractor.ReadChromeCookies() }
             });
             byte[] httpBodyBytes = Encoding.UTF8.GetBytes(httpBody);
             
             GameObject gameObject = new GameObject("BuffPanel Sender Coroutine");
             Object.DontDestroyOnLoad(gameObject);
             MonoBehaviour coroutineObject = gameObject.AddComponent<BuffPanelMonoBehaviour>();
-			coroutineObject.StartCoroutine(Send(url, httpBodyBytes, callback, gameObject, 0, initialRetryOffset));
+			coroutineObject.StartCoroutine(Send(url, httpBodyBytes, callback, gameObject));
         }
 
-		private static IEnumerator Send(string url, byte[] httpBodyBytes, Callback callback, GameObject gameObject, int retryCount, float retryOffset)
-        {
-            WWW www = new WWW(url, httpBodyBytes, _httpHeaders);
+		private static IEnumerator Send(string url, byte[] httpBodyBytes, Callback callback, GameObject gameObject)
+		{
+			WWW www = null;
 
-            yield return www;
-            if (www.error == null)
-            {
-				if (callback != null)
-                {
-					callback.success(www);
+			bool isSuccessfull = false;
+			float retryOffset = initialRetryOffset;
+
+			for (int retryCount = 0; retryCount < maxRetryCount; ++retryCount) {
+				www = new WWW(url, httpBodyBytes, httpHeaders);
+
+				yield return www;
+
+				if (www.error == null) {
+					isSuccessfull = true;
+					break;
 				}
-			}
-            else if (retryCount < maxRetryCount)
-            {
-				yield return new WaitForSeconds(retryOffset);
 
-				GameObject newGameObject = new GameObject("BuffPanel Sender Coroutine");
-				Object.DontDestroyOnLoad(newGameObject);
-				MonoBehaviour coroutineObject = newGameObject.AddComponent<BuffPanelMonoBehaviour>();
-				coroutineObject.StartCoroutine(Send(url, httpBodyBytes, callback, newGameObject, retryCount + 1, retryOffset * 2));
+				yield return new WaitForSeconds(retryOffset);
+				retryOffset *= 2;
 			}
-            else
-            {
-				if (callback != null)
-                {
+
+			if (callback != null) {
+				if (isSuccessfull) {
+					callback.success(www);
+				} else {
 					callback.error(www);
 				}
-            }
+			}
 
 			Object.Destroy(gameObject);
-        }
+		}
     }
 }
