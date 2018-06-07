ae080b12fe6ab54494fe82c0adc9e59d49262b2f
Kralmajster <marekbohm13@gmail.com>
2018-06-07 17:40:00 +0200

    add UUID generation and persistence

--- Assets/Scripts/BuffPanelSDK/BuffPanel.cs
+++ Assets/Scripts/BuffPanelSDK/BuffPanel.cs
@@ -1,95 +1,196 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Text;
+using System;
+using System.Text.RegularExpressions;
+using System.IO;
 using UnityEngine;
+
 
 namespace BuffPanel
 {
-	class BuffPanelMonoBehaviour : MonoBehaviour
-	{
-	}
+    class BuffPanelMonoBehaviour : MonoBehaviour
+    {
+    }
 
-	public class BuffPanel
-	{
-		public static string serviceHostname = "api.buffpanel.com";
-		public static string servicePath = "/run_event/create";
+    public class BuffPanel
+    {
+        public static string serviceHostname = "api.buffpanel.com";
+        public static string servicePath = "/run_event/create";
 
-		public static string version = "unity_0.0.1";
+        public static string version = "unity_1.0.0";
 
-		private static float initialRetryOffset = 0.25f;
-		private static int maxRetryCount = 10;
+        private static float initialRetryOffset = 0.25f;
+        private static int maxRetryCount = 10;
 
-		private static Dictionary<string, string> httpHeaders = new Dictionary<string, string> {
-			{ "Content-type", "application/json" }
-		};
+        private static Dictionary<string, string> httpHeaders = new Dictionary<string, string> {
+            { "Content-type", "application/json" }
+        };
 
-		public static void Track(string gameToken, string playerToken, bool isRepeated, Callback callback = null)
-		{
-			string url = "http://" + serviceHostname + servicePath;
+        public static void Track(string gameToken, bool isExistingPlayer, Callback callback = null)
+        {
+            string playerToken = "";
+            try
+            {
+                playerToken = GetPlayerToken(gameToken);
+            }
+            catch (Exception)
+            {
+                playerToken = "unknown_player";
+            }
 
-			string httpBody = Json.Serialize(new Dictionary<string, object> {
-				{ "game_token", gameToken },
-				{ "player_token", playerToken },
-				{ "is_existing_player", isRepeated },
-				{ "version", version }
-			});
-			byte[] httpBodyBytes = Encoding.UTF8.GetBytes(httpBody);
+            string url = "http://" + serviceHostname + servicePath;
 
-			GameObject gameObject = new GameObject("BuffPanel Sender Coroutine");
-			Object.DontDestroyOnLoad(gameObject);
-			MonoBehaviour coroutineObject = gameObject.AddComponent<BuffPanelMonoBehaviour>();
-			coroutineObject.StartCoroutine(Send(url, httpBodyBytes, callback, gameObject));
-		}
+            string httpBody = Json.Serialize(new Dictionary<string, object> {
+                { "game_token", gameToken },
+                { "player_token", playerToken },
+                { "is_existing_player", isExistingPlayer },
+                { "version", version }
+            });
+            byte[] httpBodyBytes = Encoding.UTF8.GetBytes(httpBody);
 
-		public static void Track(string gameToken, string playerToken, bool isRepeated, Dictionary<string, string> attributes, Callback callback = null)
-		{
-			string url = "http://" + serviceHostname + servicePath;
+            GameObject gameObject = new GameObject("BuffPanel Sender Coroutine");
+            UnityEngine.Object.DontDestroyOnLoad(gameObject);
+            MonoBehaviour coroutineObject = gameObject.AddComponent<BuffPanelMonoBehaviour>();
+            coroutineObject.StartCoroutine(Send(url, httpBodyBytes, callback, gameObject));
+        }
 
-			string httpBody = Json.Serialize(new Dictionary<string, object> {
-				{ "game_token", gameToken },
-				{ "player_token", playerToken },
-				{ "is_existing_player", isRepeated },
-				{ "attributes", attributes },
-				{ "version", version }
-			});
-			byte[] httpBodyBytes = Encoding.UTF8.GetBytes(httpBody);
+        public static void Track(string gameToken, bool isExistingPlayer, Dictionary<string, string> attributes, Callback callback = null)
+        {
 
-			GameObject gameObject = new GameObject("BuffPanel Sender Coroutine");
-			Object.DontDestroyOnLoad(gameObject);
-			MonoBehaviour coroutineObject = gameObject.AddComponent<BuffPanelMonoBehaviour>();
-			coroutineObject.StartCoroutine(Send(url, httpBodyBytes, callback, gameObject));
-		}
+            string playerToken = "";
+            try
+            {
+                playerToken = GetPlayerToken(gameToken);
+            }
+            catch (Exception)
+            {
+                playerToken = "unknown_player";
+            }
 
-		private static IEnumerator Send(string url, byte[] httpBodyBytes, Callback callback, GameObject gameObject)
-		{
-			WWW www = null;
+            string url = "http://" + serviceHostname + servicePath;
 
-			bool isSuccessfull = false;
-			float retryOffset = initialRetryOffset;
+            string httpBody = Json.Serialize(new Dictionary<string, object> {
+                { "game_token", gameToken },
+                { "player_token", playerToken },
+                { "is_existing_player", isExistingPlayer },
+                { "attributes", attributes },
+                { "version", version }
+            });
+            byte[] httpBodyBytes = Encoding.UTF8.GetBytes(httpBody);
 
-			for (int retryCount = 0; retryCount < maxRetryCount; ++retryCount) {
-				www = new WWW(url, httpBodyBytes, httpHeaders);
+            GameObject gameObject = new GameObject("BuffPanel Sender Coroutine");
+            UnityEngine.Object.DontDestroyOnLoad(gameObject);
+            MonoBehaviour coroutineObject = gameObject.AddComponent<BuffPanelMonoBehaviour>();
+            coroutineObject.StartCoroutine(Send(url, httpBodyBytes, callback, gameObject));
+        }
 
-				yield return www;
+        private static IEnumerator Send(string url, byte[] httpBodyBytes, Callback callback, GameObject gameObject)
+        {
+            WWW www = null;
 
-				if (www.error == null) {
-					isSuccessfull = true;
-					break;
-				}
+            bool isSuccessfull = false;
+            float retryOffset = initialRetryOffset;
 
-				yield return new WaitForSeconds(retryOffset);
-				retryOffset *= 2;
-			}
+            for (int retryCount = 0; retryCount < maxRetryCount; ++retryCount)
+            {
+                www = new WWW(url, httpBodyBytes, httpHeaders);
 
-			if (callback != null) {
-				if (isSuccessfull) {
-					callback.success(www);
-				} else {
-					callback.error(www);
-				}
-			}
+                yield return www;
 
-			Object.Destroy(gameObject);
-		}
-	}
+                if (www.error == null)
+                {
+                    isSuccessfull = true;
+                    break;
+                }
+
+                yield return new WaitForSeconds(retryOffset);
+                retryOffset *= 2;
+            }
+
+            if (callback != null)
+            {
+                if (isSuccessfull)
+                {
+                    callback.success(www);
+                }
+                else
+                {
+                    callback.error(www);
+                }
+            }
+
+            UnityEngine.Object.Destroy(gameObject);
+        }
+
+        private static string GetUuidPersistPath()
+        {
+            OperatingSystem os = Environment.OSVersion;
+            PlatformID platform = os.Platform;
+            switch (platform)
+            {
+                case PlatformID.Win32NT:
+                case PlatformID.Win32S:
+                case PlatformID.Win32Windows:
+                case PlatformID.WinCE:
+                    return Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\BuffPanel\";
+                case PlatformID.Unix:
+                    return Path.GetDirectoryName(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)) + @"/BuffPanel/";
+                case PlatformID.MacOSX:
+                    return Path.GetDirectoryName(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)) + @"/BuffPanel/";
+                default:
+                    return "";
+            }
+        }
+
+        private static string ReadSavedUuid(string path)
+        {
+            if (System.IO.File.Exists(path))
+            {
+                string uuid;
+                try
+                {
+                    uuid = System.IO.File.ReadAllText(path);
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    return "anonymous";
+                }
+                catch (Exception)
+                {
+                    return "";
+                }
+                if (!IsValidUuid(uuid))
+                    return "";
+
+                return uuid;
+            }
+            return "";
+        }
+
+        private static void SaveUuid(string filePath, string folderPath, string uuid)
+        {
+            System.IO.Directory.CreateDirectory(folderPath);
+            System.IO.File.WriteAllText(filePath, uuid);
+        }
+
+        private static string GetPlayerToken(string gameToken)
+        {
+            string folderPath = GetUuidPersistPath();
+            string filePath = folderPath + "uuid_" + gameToken;
+            string uuid = ReadSavedUuid(filePath);
+            if (string.IsNullOrEmpty(uuid))
+            {
+                uuid = System.Guid.NewGuid().ToString("D").ToUpper();
+                SaveUuid(filePath, folderPath, uuid);
+            }
+            return uuid;
+        }
+
+        private static bool IsValidUuid(string uuid)
+        {
+            Regex uuidRegex = new Regex(@"^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$");
+            return uuidRegex.IsMatch(uuid);
+        }
+    }
 }
